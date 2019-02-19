bf9ec86662cba260b2ef4b63caa0f82abca7c9a2
Colton G. Rushton <colton51919@gmail.com>
2019-02-19 11:04:25 -0400

    Added functionality to the purge command
    Added the ability for the purge command to delete it's own message after 5 seconds.

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -11,15 +11,27 @@ class PurgeMessages extends commando.Command {
 		});
     }
     async run(message,args){
-        let params = message.content.split(/ +/).slice(1)
+        let params
+        if (message.guild.commandPrefix==null || message.guild.commandPrefix==undefined){
+            params = message.content.split(/ +/).slice(bot.commandPrefix.length)
+        }else{
+            params = message.content.split(/ +/).slice(message.guild.commandPrefix.length)
+        }
         var amount=parseInt(params[0])
         if(!params[0]){
             message.channel.send("You need to specify the amount of messages to delete!\nExample: `&purge 10`")
             return
         }else if (params[0]){
             tools.purge(message, bot, amount)
-            message.channel.send('Successfully deleted '+params[0]+' messages.')
-            return
+            if (amount=="1"){
+                message.channel.send('Successfully deleted '+params[0]+' message.')
+                setTimeout(function(){tools.purge(message, bot, 1)}, 5000)
+                return
+            }else{
+                message.channel.send('Successfully deleted '+params[0]+' messages.')
+                setTimeout(function(){tools.purge(message, bot, 1)}, 5000)
+                return
+            }
         }else{
             message.reply('you do not have permission to use this command. Either you, the bot, or both are missing the MANAGE_MESSAGES permission.')
         }
