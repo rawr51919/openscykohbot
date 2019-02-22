e95d7310a173cc8b710ad5f30429f1e598e25702
Colton G. Rushton <colton51919@gmail.com>
2019-02-22 12:18:51 -0400

    Add files via upload
    Changed the way the purge command checks for valid numbers.

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -13,10 +13,14 @@ class PurgeMessages extends commando.Command {
         if (message.channel.type!=='dm'){
             args = message.content.split(/ +/).slice(message.guild.commandPrefix.length)
         }
+        var intRegex = /^\d+$/
         if(!args){
-            message.channel.send("You need to specify the amount of messages to delete!\nExample: `&purge 10`\nExample in DMs (this will only delete the bot's messages, not your own): `@OpenScykohBot purge 10`")
+            message.channel.send("You need to specify the amount of messages to delete.\nExample: `&purge 10`\nExample in DMs (this will only delete the bot's messages, not your own): `@OpenScykohBot purge 10`")
             return
-        }else if (args && message.channel.type!=='dm'){
+        }else if(!intRegex.test(args)){
+            message.channel.send("I can't delete messages without a proper number. Try again with a proper number.")
+            return
+        }else if (intRegex.test(args) && message.channel.type!=='dm'){
             tools.purge(message, this.client, parseInt(args))
             if (args==1){
                 message.channel.send('Successfully deleted '+args+' message.').then(message => message.delete(3000))
@@ -25,7 +29,7 @@ class PurgeMessages extends commando.Command {
                 message.channel.send('Successfully deleted '+args+' messages.').then(message => message.delete(3000))
                 return
             }
-        }else if (args && message.channel.type==='dm'){
+        }else if (intRegex.test(args) && message.channel.type==='dm'){
             if (args>100){
                 message.channel.send("I can only delete up to 100 of my DM messages at a time.\nLower the message amount and try again, as you're gonna probably overload both me and the API.\nNo one wants that, do we?")
             }else{
