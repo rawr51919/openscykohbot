d476be3f038d8779263da0871d2daf3119241bb7
Colton G. Rushton <colton51919@gmail.com>
2019-02-21 23:05:26 -0400

    Add files via upload
    Trim some of the fat off of the rps command.

--- commands/random/rps.js
+++ commands/random/rps.js
@@ -21,45 +21,45 @@ class RockPaperScissors extends commando.Command {
 		}
 		if(args[0].toLowerCase() == 'rock') {
 			if(choice == 0) {
-				message.channel.send(":fist::skin-tone-1: - I choose rock. It's a tie.")
+				message.channel.send(":fist::skin-tone-1: I choose rock. It's a tie.")
 			} else if(choice == 1) {
-				message.channel.send(":hand_splayed::skin-tone-1: - I choose paper. I win!")
+				message.channel.send(":hand_splayed::skin-tone-1: I choose paper. I win!")
 			} else if(choice == 2) {
-				message.channel.send(":v::skin-tone-1: - I choose scissors. You win!")
+				message.channel.send(":v::skin-tone-1: I choose scissors. You win!")
 			} else if(choice == 3) {
-				message.channel.send(":hand_splayed::skin-tone-1: - I choose paper. I win!")
+				message.channel.send(":hand_splayed::skin-tone-1: I choose paper. I win!")
 			} else if(choice == 4) {
-				message.channel.send(":fist::skin-tone-1: - I choose rock. It's a tie.")
+				message.channel.send(":fist::skin-tone-1: I choose rock. It's a tie.")
 			} 
 		} else if(args[0].toLowerCase() == 'paper') {
 			if(choice == 0) {
-				message.channel.send(":fist::skin-tone-1: - I choose rock. You win!")
+				message.channel.send(":fist::skin-tone-1: I choose rock. You win!")
 			} else if(choice == 1) {
-				message.channel.send(":hand_splayed::skin-tone-1: - I choose paper. It's a tie.")
+				message.channel.send(":hand_splayed::skin-tone-1: I choose paper. It's a tie.")
 			} else if(choice == 2) {
-				message.channel.send(":v::skin-tone-1: - I choose scissors. I win!")
+				message.channel.send(":v::skin-tone-1: I choose scissors. I win!")
 			} else if(choice == 3) {
-				message.channel.send(":hand_splayed::skin-tone-1: - I choose paper. It's a tie.")
+				message.channel.send(":hand_splayed::skin-tone-1: I choose paper. It's a tie.")
 			} else if(choice == 4) {
-				message.channel.send(":v::skin-tone-1: - I choose scissors. I win!")
+				message.channel.send(":v::skin-tone-1: I choose scissors. I win!")
 			}
 		} else if(params[0].toLowerCase() == 'scissors') {
 			if(choice == 0) {
-				message.channel.send(":fist::skin-tone-1: - I choose rock. I win!")
+				message.channel.send(":fist::skin-tone-1: I choose rock. I win!")
 			} else if(choice == 1) {
-				message.channel.send(":hand_splayed::skin-tone-1: - I choose paper. You win!")
+				message.channel.send(":hand_splayed::skin-tone-1: I choose paper. You win!")
 			} else if(choice == 2) {
-				message.channel.send(":v::skin-tone-1: - I choose scissors. It's a tie.")
+				message.channel.send(":v::skin-tone-1: I choose scissors. It's a tie.")
 			} else if(choice == 3) {
-				message.channel.send(":v::skin-tone-1: - I choose scissors. It's a tie.")
+				message.channel.send(":v::skin-tone-1: I choose scissors. It's a tie.")
 			} else if(choice == 4) {
-				message.channel.send(":fist::skin-tone-1: - I choose rock. I win!")
+				message.channel.send(":fist::skin-tone-1: I choose rock. I win!")
 			}
 		} else if(args[0].toLowerCase() == 'tornado') {
-			message.channel.send(":dash: - I choose hurricane!")
+			message.channel.send("I choose hurricane! :dash:")
 			return
 		} else if(args[0].toLowerCase() == 'hurricane') {
-			message.channel.send(":cloud_tornado: - I choose tornado!")
+			message.channel.send("I choose tornado! :cloud_tornado:")
 			return
 		} else {
 			message.channel.send("You need to specify if you're playing Rock, Paper, Scissors, Tornado, or Hurricane.\nExample: `&rps rock`")
