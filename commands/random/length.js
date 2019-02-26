47c000f5344e6327f1576aa97a33edb83fcb8672
Colton G. Rushton <colton51919@gmail.com>
2019-02-26 09:09:57 -0400

    Add files via upload
    Fixed the length command.

--- commands/random/length.js
+++ commands/random/length.js
@@ -1,24 +1,34 @@
 const commando = require('discord.js-commando')
 class StringLength extends commando.Command {
-        constructor(client) {
-            super(client, {
-                name: 'length',
-                group: 'random',
-                memberName: 'length',
-                description: 'Computes the length of whatever you specify.',
-            })
-        }
-        async run(message,args){
-                if (message.channel.type!=='dm')
-                    args = message.content.split(/ +/).slice(message.guild.commandPrefix.length)
-                if (args[0] && args.length==1){
-                    message.channel.send(args[0]+' contains '+args[0].length+' characters.')
-                }else if (args.length>=2){
-                    for (var i=0;i<=args.length;i++){
-                        message.channel.send(args[i]+' contains '+args[i].length+' characters.')
-                    }
-                    message.channel.send('The combined length of those strings is '+args.length+' characters.')
+    constructor(client) {
+        super(client, {
+            name: 'length',
+            group: 'random',
+            memberName: 'length',
+            description: 'Computes the length of whatever you specify.',
+        })
+    }
+    async run(message,args){
+        if (message.channel.type!=='dm')
+            args = message.content.split(/ +/).slice(message.guild.commandPrefix.length)
+        var arglength=0
+        if (!args[0]){
+            message.reply('You need to specify a string to get the length of. Example: `&length <string>`')
+        }else if (args[0] && args.length==1 && args[0].length==1){
+            message.channel.send(args[0]+' contains '+args[0].length+' character.')
+        }else if (args[0] && args.length==1 && args[0].length>1){
+            message.channel.send(args[0]+' contains '+args[0].length+' characters.')
+        }else if (args.length>=2){
+            for (var i=0;i<=args.length-1;i++){
+                if (args[i].length==1){
+                    message.channel.send(args[i]+' contains '+args[i].length+' character.')
+                }else{
+                    message.channel.send(args[i]+' contains '+args[i].length+' characters.')
                 }
+                arglength=arglength+args[i].length
+            }
+            message.channel.send('The combined length of those strings is '+arglength+' characters.')
         }
+    }
 }
 module.exports=StringLength
