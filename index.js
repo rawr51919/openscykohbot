283f275f04c410f5fc22273cc42af105ffa0dc47
rawr51919 <colton51919@gmail.com>
2025-08-18 15:11:24 -0300

    Harden the bot against emoji reactions using emoji not found in guilds or the bot's own cache

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, DiscordAPIError } = require("discord.js");
 const Random = require("random-js");
 const random = new Random.Random();
 
@@ -14,6 +14,17 @@ const bot = new Client({
 });
 
 // ---------------------------
+// Emoji error handler
+// ---------------------------
+function handleEmojiError(err) {
+  if (err instanceof DiscordAPIError && err.code === 10014) {
+    console.warn("Can't find emoji in guild or cache.");
+  } else {
+    console.error(err);
+  }
+}
+
+// ---------------------------
 // Ready event
 // ---------------------------
 bot.once("ready", () => {
@@ -21,61 +32,62 @@ bot.once("ready", () => {
   bot.user.setStatus("online");
   bot.user.setPresence({
     activities: [
-      {
-        name: "Scykoh... wait...",
-        type: 3
-      }
+      { name: "Scykoh... wait...", type: 3 }
     ]
   });
 });
+
+// ---------------------------
+// Handle reactions
+// ---------------------------
+function handleReactions(msg, message) {
+  const reactMap = [
+    { trigger: "amirite", reacts: ["🇳", "🇴"], delay: [0, 1000] },
+    { trigger: "rip", reacts: ["⚰"] },
+    { trigger: "wh", reacts: ["scyWh:548503602113544193"] },
+  ];
+
+  for (const r of reactMap) {
+    if (msg.includes(r.trigger)) {
+      r.reacts.forEach((emoji, i) =>
+        setTimeout(() => message.react(emoji).catch(handleEmojiError), r.delay?.[i] || 0)
+      );
+    }
+  }
+
+  if (message.author.id === "159985870458322944") {
+    ["🇸", "🇭", "🇺", "🇹", "🆙"].forEach((emoji, i) =>
+      setTimeout(() => message.react(emoji).catch(handleEmojiError), i * 1000)
+    );
+  }
+}
 
 // ---------------------------
 // Message handler
 // ---------------------------
 bot.on("messageCreate", (message) => {
   const msg = message.content.toLowerCase();
-
-  // Ignore bot messages
   if (message.author.bot) return;
 
-  // ---------------------------
   // Predefined talks
-  // ---------------------------
   const predefinedTalks = {
     sad: {
       triggers: ["bad openscykohbot", "bad <@468615764643938314>"],
-      messages: [
-        "It's all my fault.",
-        "It's not my fault! It's random!",
-        ";(",
-        "Sorry :(",
-        "That kinda hurt my feelings...",
-        "no u"
-      ]
+      messages: ["It's all my fault.", "It's not my fault! It's random!", ";(", "Sorry :(", "That kinda hurt my feelings...", "no u"]
     },
     happy: {
       triggers: ["good openscykohbot", "good <@468615764643938314>"],
-      messages: [
-        "I'm always bad.",
-        "I'm always bad. That's what bots do.",
-        "Bots aren't good. Especially me.",
-        "Okay, never heard that one before. People are usually spamming \"bad openscykohbot\"...",
-        "Uhh... okay?",
-        ":)",
-        ";)"
-      ]
+      messages: ["I'm always bad.", "I'm always bad. That's what bots do.", "Bots aren't good. Especially me.", "Okay, never heard that one before. People are usually spamming \"bad openscykohbot\"...", "Uhh... okay?", ":)", ";)"]
     }
   };
 
-  Object.values(predefinedTalks).forEach(group => {
+  for (const group of Object.values(predefinedTalks)) {
     if (group.triggers.includes(msg)) {
       message.channel.send(group.messages[random.integer(0, group.messages.length - 1)]);
     }
-  });
+  }
 
-  // ---------------------------
   // Predefined responses
-  // ---------------------------
   const responses = {
     "how's it going, openscykohbot?": "It's not easy being a bot... ~~listening to your demands~~",
     "<@468615764643938314> go away": "NEVAR!!",
@@ -103,9 +115,7 @@ bot.on("messageCreate", (message) => {
     message.channel.send("Hi, <@" + message.author.id + ">.");
   }
 
-  // ---------------------------
   // Foul bot responses
-  // ---------------------------
   const foulTriggers = [
     "<@468615764643938314> speak to me, foul bot!",
     "<@468615764643938314>, speak to me, foul bot!"
@@ -113,35 +123,15 @@ bot.on("messageCreate", (message) => {
 
   if (foulTriggers.includes(msg)) {
     if (message.author.id === "246865356482805760") {
-      const crusadermsgs = [
-        "Greetings, Sir Crusader.",
-        "Hast thou found me irritating?",
-        "Dost thou want to fight?"
-      ];
+      const crusadermsgs = ["Greetings, Sir Crusader.", "Hast thou found me irritating?", "Dost thou want to fight?"];
       message.channel.send(crusadermsgs[random.integer(0, crusadermsgs.length - 1)]);
     } else {
       message.channel.send("No. I don't speak to the likes of you.");
     }
   }
 
-  // ---------------------------
   // Reactions
-  // ---------------------------
-  const reactMap = [
-    { trigger: "amirite", reacts: ["🇳", "🇴"], delay: [0, 1000] },
-    { trigger: "rip", reacts: ["⚰"] },
-    { trigger: "wh", reacts: ["<:scyWh:548503602113544193>"] }
-  ];
-
-  reactMap.forEach(r => {
-    if (msg.includes(r.trigger)) {
-      r.reacts.forEach((emoji, i) => setTimeout(() => message.react(emoji), r.delay ? r.delay[i] || 0 : 0));
-    }
-  });
-
-  if (message.author.id === "159985870458322944") {
-    ["🇸", "🇭", "🇺", "🇹", "🆙"].forEach((emoji, i) => setTimeout(() => message.react(emoji), i * 1000));
-  }
+  handleReactions(msg, message);
 });
 
 // ---------------------------
